3f880e4cd39bd7ca72c0c369ed478d412beb837b
Pedro Solis <pedro.h.1997z@gmail.com>
2024-05-01 00:02:21 -0600

    Commit - Corrección en cajas de texto

--- lib/widgets/cajatexto_personalizada.dart
+++ lib/widgets/cajatexto_personalizada.dart
@@ -8,52 +8,52 @@ class CajaTextoPersonalizada extends StatelessWidget {
   final IconData? icono, iconoPrefix;
   final Function(String)? onChanged;
   final String? Function(String?)? validator;
- // final String? initialValue;
+  final String? initialValue;
 
-  const CajaTextoPersonalizada({
-    super.key,
-    this.label,
-    this.hint,
-    this.errorMessage,
-    this.obscureText = false,
-    this.onChanged,
-    this.validator,
-    this.icono,
-    this.iconoPrefix,
- //   this.initialValue
-  });
+  const CajaTextoPersonalizada(
+      {super.key,
+      this.label,
+      this.hint,
+      this.errorMessage,
+      this.obscureText = false,
+      this.onChanged,
+      this.validator,
+      this.icono,
+      this.iconoPrefix,
+      this.initialValue});
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
+    //final colors = Theme.of(context).colorScheme;
 
     final border = OutlineInputBorder(
         borderSide: const BorderSide(color: Colors.black45),
         borderRadius: BorderRadius.circular(10));
 
     return TextFormField(
-    //  initialValue: initialValue,
+      initialValue: initialValue,
       onChanged: onChanged,
       validator: validator,
       obscureText: obscureText,
       autocorrect: false,
       decoration: InputDecoration(
-          enabledBorder: border,
-          focusedBorder: border.copyWith(
-              borderSide: const BorderSide(color: Colors.black45)),
-          errorBorder: border.copyWith(
-              borderSide: BorderSide(color: Colors.red.shade800)),
-          focusedErrorBorder: border.copyWith(
-              borderSide: BorderSide(color: Colors.red.shade800)),
-          isDense: true,
-          label: label != null ? Text(label!) : null,
-          labelStyle: const TextStyle(
-              color: Colors.black45, fontWeight: FontWeight.normal),
-          hintText: hint,
-          errorText: errorMessage,
-          focusColor: Colors.black,
-          suffixIcon: icono == null ? Container() : Icon(icono),
-          prefix: iconoPrefix == null ? Container() : Icon(iconoPrefix)),
+        enabledBorder: border,
+        focusedBorder: border.copyWith(
+            borderSide: const BorderSide(color: Colors.black45)),
+        errorBorder:
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        focusedErrorBorder:
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        isDense: true,
+        label: label != null ? Text(label!) : null,
+        labelStyle: const TextStyle(
+            color: Colors.black45, fontWeight: FontWeight.normal),
+        hintText: hint,
+        errorText: errorMessage,
+        focusColor: Colors.black,
+        suffixIcon: icono == null ? Container() : Icon(icono),
+        //prefix: iconoPrefix == null ? Container() : Icon(iconoPrefix)
+      ),
     );
   }
 }
